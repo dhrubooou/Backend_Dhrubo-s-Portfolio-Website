1fdd1e8126bbd81f4a0314f9cd192b21f052f8cc
DHRUBOJYOTI BHATTACHARJEE <117100651+dhrubooou@users.noreply.github.com>
2025-08-08 22:20:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,5 +37,5 @@ app.post("/send-email", async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
